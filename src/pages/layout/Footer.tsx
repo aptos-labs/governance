ab6752fab00c5fa804f458d5b25646951668e98d
Joe <98909677+moonclavedev@users.noreply.github.com>
2022-09-29 06:24:16 -0700

    Update footer to Aptos Foundation & MUI Grid v2 (#3)

--- src/pages/layout/Footer.tsx
+++ src/pages/layout/Footer.tsx
@@ -1,20 +1,17 @@
 import React from "react";
-import Typography from "@mui/material/Typography";
-import Link from "@mui/material/Link";
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
+import {Box, Container, Link, Typography, useTheme} from "@mui/material";
 
-import {ReactComponent as GithubLogo} from "../../assets/github.svg";
-import {ReactComponent as DiscordLogo} from "../../assets/discord.svg";
-import {ReactComponent as TwitterLogo} from "../../assets/twitter.svg";
-import {ReactComponent as MediumLogo} from "../../assets/medium.svg";
-import {ReactComponent as LinkedInLogo} from "../../assets/linkedin.svg";
-import Box from "@mui/material/Box";
-import {useTheme} from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2";
+
+import {ReactComponent as GithubLogo} from "../../assets/svg/github.svg";
+import {ReactComponent as DiscordLogo} from "../../assets/svg/discord.svg";
+import {ReactComponent as TwitterLogo} from "../../assets/svg/twitter.svg";
+import {ReactComponent as MediumLogo} from "../../assets/svg/medium.svg";
+import {ReactComponent as LinkedInLogo} from "../../assets/svg/linkedin.svg";
 import {grey} from "../../themes/colors/aptosColorPalette";
 import SvgIcon, {SvgIconProps} from "@mui/material/SvgIcon";
 
-import {ReactComponent as LogoFull} from "../../assets/svg/aptos_logo_full.svg";
+import {ReactComponent as LogoIcon} from "../../assets/svg/aptos-foundation_logo_mark.svg";
 
 const socialLinks = [
   {title: "Git", url: "https://github.com/aptos-labs", icon: GithubLogo},
@@ -42,55 +39,37 @@ export default function Footer() {
       <Container maxWidth="xl" sx={{paddingTop: "2rem", paddingBottom: "2rem"}}>
         <Grid
           container
-          spacing={{xs: 4, md: 1}}
+          rowSpacing={4}
+          columnSpacing={{xs: 2, sm: 2, md: 3}}
           alignContent="center"
           alignItems="center"
           direction={{xs: "column", md: "row"}}
         >
-          <Grid item xs="auto" sx={{mr: 2}} container justifyContent="start">
+          <Grid>
             <Link
               color="inherit"
               href="https://aptoslabs.com/"
               target="_blank"
-              sx={{width: "8rem"}}
+              title="Aptos Foundation"
+              sx={{display: "block", width: "3rem"}}
             >
-              <LogoFull />
+              <LogoIcon />
             </Link>
           </Grid>
-          <Grid item xs="auto" container justifyContent="start">
+          <Grid container justifyContent="start">
             <Typography
               sx={{textAlign: {xs: "center", md: "left"}}}
               fontSize="0.8rem"
             >
               © {new Date().getFullYear()}{" "}
               <Box component="span" sx={{whiteSpace: "nowrap"}}>
-                Matonee Inc. (dba Aptos Labs)
+                Aptos Foundation
               </Box>
-              <br />
-              <Link
-                color="inherit"
-                href="mailto:info@aptoslabs.com"
-                target="_blank"
-              >
-                info@aptoslabs.com
-              </Link>
-              <Box component="span" sx={{px: 1, display: "inline-block"}}>
-                or
-              </Box>
-              <Link
-                color="inherit"
-                href="mailto:press@aptoslabs.com"
-                target="_blank"
-              >
-                press@aptoslabs.com
-              </Link>
             </Typography>
           </Grid>
           <Grid
-            item
             xs="auto"
             sx={{marginLeft: {xs: "0", md: "auto"}}}
-            container
             justifyContent="end"
           >
             <Grid
@@ -100,7 +79,7 @@ export default function Footer() {
               direction="row"
             >
               {socialLinks.map((link) => (
-                <Grid item key={link.title}>
+                <Grid key={link.title}>
                   <Link
                     color="inherit"
                     href={link.url}
@@ -108,7 +87,7 @@ export default function Footer() {
                     rel="noopener noreferrer"
                     title={link.title}
                     width="26px"
-                    sx={{display: "block"}}
+                    sx={{display: "flex"}}
                   >
                     <SvgIcon component={link.icon} inheritViewBox />
                   </Link>
